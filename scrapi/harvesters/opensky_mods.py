71bf0b88209a543a0fa932b69d552f61493a6b83
Jonathan Ostwald <ostwald@ucar.edu>
2016-07-12 13:58:28 -0600

    added uris to normalized json

--- scrapi/harvesters/opensky_mods.py
+++ scrapi/harvesters/opensky_mods.py
@@ -1,10 +1,10 @@
 """
 Harvester for OpenSky v2 (http://opensky.ucar.edu/oai2)
 
-- grabs records in "osm" framework (http://nldr.library.ucar.edu/metadata/osm/1.1/schemas/osm.xsd)
+- grabs records in "mods" framework
 - extracts info to populate customized SHARE schema
 
-Example API call: http://nldr.library.ucar.edu/dds/services/oai2-0?verb=ListRecords&metadataPrefix=osm&from=2015-10-06T00:00:00Z&until=2015-10-09T00:00:00Z
+Example API call: http://opensky.ucar.edu/oai2?verb=ListRecords&metadataPrefix=mods&from=2016-07-12T00:00:00Z&until=2016-07-13T00:00:00Z
 
 """
 
@@ -28,35 +28,35 @@ class OpenskyHarvester (OAIHarvester):
 	url = 'http://opensky.ucar.edu'
 	timezone_granularity = True
 	metadata_prefix = 'mods'
-	
+
 	namespaces = {
 		'ns0': 'http://www.openarchives.org/OAI/2.0/',
 		'mods' : "http://www.loc.gov/mods/v3",
 		'xlink' : "http://www.w3.org/1999/xlink",
 		'dc': 'http://purl.org/dc/elements/1.1/'
 	}
-	
+
 	base_url = 'http://opensky.ucar.edu/oai2'
-	
+
 	# these are other properties in schema
-	property_listOFF = [
-		'type', 'date', 'setSpec',
+	property_list = [
+		'type', 'date', 'setSpec'
 		# 'source', 'format', 'identifier'
 	]
-	
+
 	@property
 	def schema(self):
 		"""
-		Custom function to define schema values in terms of OSM records. 
+		Custom function to define schema values in terms of OSM records.
 		Required fields (see https://osf.io/wur56/wiki/Schema/) are:
 		- title
 		- contributors
 		- uris
 		- providerUpdatedDateTime
-		
+
 		NOTE: contributors
 		"""
-		
+
 		xlink_desc = 'A collection of RelatedIdentifiers as specified by DataCite.'
 		xlink_uri = 'https://schema.datacite.org/meta/kernel-3/index.html'
 
@@ -68,53 +68,52 @@ class OpenskyHarvester (OAIHarvester):
 			'publisher': {'name' : ('//mods:mods/mods:recordInfo/mods:recordContentSource/node()', lambda x: x[0] if x else '')},
 			# scrapi.base.helpers.compose takes a list of functions
 			# not sure how/why, but below xpath only selects name_els with a family name:)
-			'contributors': ('//mods:mods/mods:name[mods:namePart[@type="family"]]', 
+			'contributors': ('//mods:mods/mods:name[mods:namePart[@type="family"]]',
 				 compose(lambda x: [
-					 process_contributor(entry) 
+					 process_contributor(entry)
 						for entry in x
 					 ], lambda x: x or [])),
-			
+			'uris': {
+				'canonicalUri': ('//mods:mods/mods:identifier[@displayLabel="Persistent URI"]/node()', lambda x: x[0])
+			},
+
 			# use scrapi.base.helpers.build_properties  with relatedIdentifiers function
 			# - pass description and uri values into build_properties to describe the
 			#   relatedIdentifier property properties (they describe the property itself)
 			'otherProperties': build_properties(
-				('relatedIdentifiers', relatedIdentifers, 
+				('relatedIdentifiers', relatedIdentifers,
 					{'description' : xlink_desc, 'uri':xlink_uri}
 				)
-			),
-
+			)
 		})
-	
+
 	def harvest(self, start_date=None, end_date=None):
 		"""
 		Override OAIHarvester.harvest for two reasons
-		
+
 		1 - need to get metadata in osm format.
 			thought i could simply override META_PREFIX_DATE (see above) but
 			apparently this var is never used!
-			
-			instead we use: 
-				url.args['metadataPrefix'] = 'osm'
-		
-		2 - I wanted to understand how to use a custom date range (so I know we'll get something from
-			the provider). 
-		
 
-		NOTE: if harvester is using oai_dc it is probably not necessary to override harvest()!
-		This was done mainly as a learning exercise
-		
+			instead we use:
+				url.args['metadataPrefix'] = 'osm'
+
+		2 - I wanted to understand how to use a custom date range for testing (so I can ensure we'll get something from
+			the provider).
+
+
 		"""
-		
-		# start_date = (start_date or date.today() - timedelta(settings.DAYS_BACK)).isoformat()
-		
-		start_date = (date.today() - timedelta(3)).isoformat()
+
+		start_date = (start_date or date.today() - timedelta(settings.DAYS_BACK)).isoformat()
+
+		# start_date = (date.today() - timedelta(3)).isoformat() # DEVEL
 		end_date = (end_date or date.today()).isoformat()
 
 		if self.timezone_granularity:
 			start_date += 'T00:00:00Z'
 			end_date += 'T00:00:00Z'
 
-		# DEVEL - to fetch cross-link records
+		# DEVEL - to fetch ONLY cross-link records
 		start_date = '2016-07-12T13:47:00Z'
 		end_date = '2016-07-12T13:48:00Z'
 
@@ -126,7 +125,7 @@ class OpenskyHarvester (OAIHarvester):
 
 		records = self.get_records(url.url, start_date, end_date)
 		# print '%d records harvested' % len(records)
-		
+
 		rawdoc_list = []
 		for record in records:
 			doc_id = record.xpath(
@@ -141,7 +140,7 @@ class OpenskyHarvester (OAIHarvester):
 
 		return rawdoc_list
 
-def getXpath (base_el, xpath, namespaces=None):
+def valueOf (base_el, xpath, namespaces=None):
 	namespaces = namespaces or OpenskyHarvester.namespaces
 	try:
 		return base_el.xpath(xpath,
@@ -153,7 +152,7 @@ def getXpath (base_el, xpath, namespaces=None):
 def process_contributor(name_el):
 	"""
 	populate schema element by plucking info from supplied name_el
-	
+
 	sample name_el:
 		<name type="personal">
 			<namePart type="given">Kevin</namePart>
@@ -169,18 +168,16 @@ def process_contributor(name_el):
 				(ESSL)</affiliation>
 		</name>
 
-	
-	NOTE: see schema (https://osf.io/wur56/wiki/Schema/) - I "think" we are producing the right json
-	structure for "person" but mostly this is just a DEMO of how to get info from provided metadata
-	and create a json structure
+
+	NOTE: see schema (https://osf.io/wur56/wiki/Schema/)
 	"""
-	
-	role = getXpath(name_el, "mods:role/mods:roleTerm[@type='text']")
-	last = getXpath(name_el, "mods:namePart[@type='family']")
-	first = getXpath(name_el, "mods:namePart[@type='given']")
-	name = getXpath(name_el, "mods:displayForm")
-	affiliation = getXpath(name_el, "mods:affiliation")
-			
+
+	role = valueOf(name_el, "mods:role/mods:roleTerm[@type='text']")
+	last = valueOf(name_el, "mods:namePart[@type='family']")
+	first = valueOf(name_el, "mods:namePart[@type='given']")
+	name = valueOf(name_el, "mods:displayForm")
+	affiliation = valueOf(name_el, "mods:affiliation")
+
 	if 0:
 		print etree.tostring(name_el);
 		print ' - first: %s' % first
@@ -188,10 +185,9 @@ def process_contributor(name_el):
 		print ' - name: %s' % name
 		print ' - role: %s' % role
 		print ' - affiliation: %s\n' % affiliation
-			
-			
+
+
 	return {
-		# 'name': name,
 		'name': '%s, %s' % (last, first),
 		'givenName': first,
 		'familyName': last,
@@ -202,16 +198,9 @@ def process_contributor(name_el):
 
 def relatedIdentifers(record):
 	"""
-	DEMO of how we would pluck the following attributes from metadate record for
-	the "relatedIdentifiers" property
-	
-	- relationIdentifier
-	- relationIdentifierType (e.g., DOI)
-	- relationType (e.g., hasDataset - but something in datacite vocab?)
-	
-	and put them in the return object.
-	NOTE: the required metadata for relatedIdentifiers is not yet in osm records,
-	so I am looking for any old "doi" identifier instead ...
+	pluck information for cross-links from metadata record for
+	the custom "relatedIdentifiers" property
+
 	"""
 
 	relationType = 'UNKOWN'
@@ -226,9 +215,9 @@ def relatedIdentifers(record):
 		if relatedItem is None:
 			raise Exception, 'relatedItem not found'
 		relationType = relatedItem.get('otherType')
-		title = getXpath(relatedItem, 'mods:titleInfo/mods:title')
-		publisher = getXpath(relatedItem, 'mods:originInfo/mods:publisher')
-		genre = getXpath(relatedItem, 'mods:genre[@authorityURI="http://dx.doi.org/10.5438/0010"]')
+		title = valueOf(relatedItem, 'mods:titleInfo/mods:title')
+		publisher = valueOf(relatedItem, 'mods:originInfo/mods:publisher')
+		genre = valueOf(relatedItem, 'mods:genre[@authorityURI="http://dx.doi.org/10.5438/0010"]')
 
 		# try to get a DOI, if we can't get doi look for uri
 		identityEls = relatedItem.xpath('mods:identifier', namespaces=ns)
@@ -240,7 +229,7 @@ def relatedIdentifers(record):
 
 	except Exception, msg:
 		# maybe not really an error - we just didn't find any relation stuff'
-		print 'ERROR: %s' % msg
+		# print 'WARNING: %s' % msg
 		return {}
 
 	return {
@@ -251,12 +240,4 @@ def relatedIdentifers(record):
 		'publisher' : publisher,
 		'resourceType' : genre
 	}
-
-
-def publisher (record):
-	"""
-	'//mods:mods/mods:recordInfo/mods:recordContentSource
-	"""
-	# return {'name':'fooberry'}
-	return "fooberry"
 
